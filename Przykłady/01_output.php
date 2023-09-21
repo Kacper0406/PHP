653357ea3cac39ed44329c5487496de377d6c28a
kacper0406 <z.kacper2006@gmail.com>
2023-09-21 10:20:32 +0200

    Commit

--- Przykłady/01_output.php
+++ Przykłady/01_output.php
@@ -1,42 +1,63 @@
-<?php
+<?php // To jest tag php. Jeśli pod php nie ma html lub innej zawartości, nie musimy zamykać tagu php.
 
-echo 'Hello!';
+/* ------- Wyprowadzanie treści ------- */
+
+// Echo - Wyjściowe ciągi, liczby, html itp.
+echo 'Hello';
 echo 123;
 echo '<h1>Hello</h1>';
 
+// print - Podobny do echa, ale nieco wolniejszy
+print 'Hello';
 echo '<hr>';
 
+// print_r - Daje trochę więcej informacji. Może być używany do drukowania tablic
 print_r('Hello');
 echo '<hr>';
-print_r([1,2,3]);
+print_r([1, 2, 3]);
 echo '<hr>';
-
+// var_dump - Jeszcze więcej informacji, takich jak typ danych i długość
 var_dump('Hello');
 echo '<hr>';
-var_dump([1,2,3]);
+var_dump([1, 2, 3]);
 echo '<hr>';
+// Wprowadzanie znaków z odwrotnym ukośnikiem
+echo "Tytuł ulubionej książki to: \"Pan Tadeusz\"";
 
-echo "Tytuł twojej ulubionej ksiązki to: \"nPan Tadeusz\"";
 echo '<hr>';
-
-
+// apostrofy i cudzysłowia
 $haslo = 'toster_elektryczny';
 
-echo 'tutaj pojawi sie łancuch znaków: $haslo<br>';
-echo "tutaj pojawi sie wartość zmiennej: $haslo<br>";
+echo 'tutaj pojawi się łańcuch znaków: $haslo<br>';
+echo "tutaj pojawi się wartość zmiennej: $haslo<br>";
 
+
+/* ------------ Komentarze ------------ */
+
+// Komentarz liniowy
+
+/*
+      * Komentarz blokowy
+      *
+      * Może składać się e z wielu linii.
+      */
+
+// Jeśli po PHP jest więcej treści, na przykład ten plik, potrzebujesz znacznika końcowego. W przeciwnym razie nie.
 ?>
 
-<!doctype html>
+<!-- Możesz wyprowadzić dowolny kod HTML, który chcesz w pliku .php -->
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moja strona PHP</title>
 </head>
 <body>
+<!-- Można umieszczać kod php wewnątrz znaczników HTML -->
 <h1>Witaj <?php echo 'Roman'; ?>!!!</h1>
+<!-- Możesz pominąć średnik po instrukcji tylko wtedy, gdy bezpośrednio po niej następuje zamykający tag PHP ?>. -->
+<h1>Witaj <?= 'Marek' ?></h1>
 </body>
 </html>
